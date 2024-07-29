c26e36050666f214641411cf3f884a0426d59864
Miki Iizuka <231048@st.yoshida-g.ac.jp>
2024-07-29 12:01:45 +0900

    admin修正・企業マイページレイアウトのみ

--- iizuka/admin_top.php
+++ iizuka/admin_top.php
@@ -16,17 +16,17 @@
     <h3>ブロックワード編集</h3>
     <div class="brock_word">
         <!-- #のところに遷移するところのパス入れてください -->
-        <button class="button" onclick="location.href='#'">追加</button>
-        <button class="button" onclick="location.href='#'">編集</button>
-        <button class="button" onclick="location.href='#'">削除</button>
+        <button class="admin_button" onclick="location.href='#'">追加</button>
+        <button class="admin_button" onclick="location.href='#'">編集</button>
+        <button class="admin_button" onclick="location.href='#'">削除</button>
     </div>
 
     <h3>利用者関連</h3>
     <div class="user_con">
-        <button class="button" onclick="location.href='#'">通報履歴</button>
-        <button class="button" onclick="location.href='#'">企業承認</button>
-        <button class="button" onclick="location.href='#'">パスワードリセット</button>
-        <button class="button" onclick="location.href='#'">タグ編集</button>
+        <button class="admin_button" onclick="location.href='#'">通報履歴</button>
+        <button class="admin_button" onclick="location.href='#'">企業承認</button>
+        <button class="admin_button" onclick="location.href='#'">パスワードリセット</button>
+        <button class="admin_button" onclick="location.href='#'">タグ編集</button>
     </div>
 </body>
 
---
+++ iizuka/com_mypage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" , href="com_mypage.css">
+</head>
+
+<body>
+    <header>
+        <!-- headerを作って持ってくるで！！ -->
+    </header>
+    
+    <h2>~~~様</h2>
+    
+    <div class="mypage_boo">
+        <button class="cmypage_button" onclick="location.href='#'">オファーリスト</button>
+        <button class="cmypage_button" onclick="location.href='#'">イベント</button>
+        <button class="cmypage_button" onclick="location.href='#'">ページ編集</button>
+        <button class="cmypage_button" onclick="location.href='#'">設定</button>
+    </div>
+</body>
+
+</html>
