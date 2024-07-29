bf32b7c8884ab306c98c8b04e647cdbeb333a5c8
Miki Iizuka <231048@st.yoshida-g.ac.jp>
2024-07-29 10:29:48 +0900

    トップページ概要のみ

---
+++ iizuka/com_top.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" , href="com_top.css">
+</head>
+
+<body>
+    <header>
+        <!-- headerを作って持ってくるで！！ -->
+    </header>
+    <a class="calendar">カレンダー予定</a>
+    <div class="talk">
+        <h3 class="talk_t">トークルーム予定</h3>
+    </div>
+    <!-- <h3 class="tag">タグ追加・更新</h3> -->
+    <button class="tag">タグ追加・更新予定</button>
+</body>
+
+</html>
