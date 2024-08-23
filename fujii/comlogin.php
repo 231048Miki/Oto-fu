760978c78e922dde040e30788d5b0546a0b4f0a5
Issei Fujii <231109@st.yoshida-g.ac.jp>
2024-08-23 10:05:51 +0900

    ログイン関係のCSS

--- fujii/comlogin.php
+++ fujii/comlogin.php
@@ -7,7 +7,8 @@ $HTML_HEADER = <<<___EOF___
         <html>
         <head>
             <meta charset="UTF-8">
-            <title>ログイン画面</title>     
+            <link rel="stylesheet" , href="login.css">
+            <title>企業ログイン画面</title>     
         </head>
         <body>
     ___EOF___;
@@ -21,16 +22,22 @@ $HTML_BODY = <<<___EOF___
     <a href="">就活アプリ</a>
     <a href="login.php">学生の方はこちら</a>
     <h1>企業ログイン</h1>
+    <div class="upper">
     <form method="POST" action="comlogincheck.php">
         <p class="text">メールアドレス：<input type='text' name='mail'  value='' required></p>
         <p class="text">パスワード：<input type='password' name='pass'  value='' required></p>
+        </div>
+        <div class="under">
+        <div class="sign">
         <a href="comsignup.php">新規登録はこちら</a>
+        </div>
+        <div class="login">
         <p><input type="submit" class="submit" name="comlogin"></p>
 
-        
+        </div>
+        </div>
 
     </form>
-    </div>
     ___EOF___;
 
 echo $HTML_HEADER;
