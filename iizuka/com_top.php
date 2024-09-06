0fb50a945fa23cf33f7980508c184f26ed4e28d9
Miki Iizuka <231048@st.yoshida-g.ac.jp>
2024-09-06 09:37:28 +0900

    ヘッダーいれた

--- iizuka/com_top.php
+++ iizuka/com_top.php
@@ -4,16 +4,24 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" , href="com_top.css">
+    <link rel="stylesheet" , href="header.css">
     <!-- <title>企業用トップページ</title> -->
 </head>
 
 <body>
-    <header>
-        <!-- headerを作って持ってくるで！！ -->
-    </header>
-    
-    <div class="calender">
-        <a class="calendar-val">カレンダー予定</a>
+<div class="header">
+        <h2 class="sns" onclick="適当にいれてね">job hunting</h2>
+        <div class="menu">
+            <a href="#" class="header-nav">マイページ</a>
+            <a onclick="history.back(-1)" class="header-nav">戻る</a>
+            <a href="#" class="header-nav">ログアウト</a>
+        </div>
+    </div>
+
+    <div class="flex1">
+        <?php
+        require_once("../shirasaki/myCalendar.php");
+        ?>
     </div>
     <div class="talk">
         <h3 class="talk_t">トークルーム予定</h3>
