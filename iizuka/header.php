e23c5f8b320c733c79ee272713abc67848c96667
Miki Iizuka <231048@st.yoshida-g.ac.jp>
2024-10-02 14:35:21 +0900

    ハンバーガーメニュー

--- iizuka/header.php
+++ iizuka/header.php
@@ -4,21 +4,35 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" , href="header.css">
+    <meta name="viewport" content="width=device-width" />
 </head>
 
 <body>
-    <div class="header">
-        <h2>
-            <a href="適当にいれてね" class="web-name">job hunting</a>
-        </h2>
-        <div class="menu">
-            <a href="#" class="header-nav">マイページ</a>
-            <a onclick="history.back(-1)" class="header-nav">戻る</a>
-            <!-- <a href="#" class="header-nav">登録</a> -->
-            <!-- <a href="#" class="header-nav">ログイン</a> -->
-            <a href="#" class="header-nav">ログアウト</a>
+    <header>
+        <div class="header">
+            <h2>
+                <a href="適当にいれてね" class="web-name">job hunting</a>
+            </h2>
+            <div class="menu">
+                <div id="nav-drawer">
+                    <input id="nav-input" type="checkbox" class="nav-unshown">
+                    <label id="nav-open" for="nav-input" class="nav-unshown"><span></span></label>
+                    <label class="nav-unshown" id="nav-close" for="nav-input"></label>
+
+                    <div id="nav-content">
+                        <a href="#" class="header-nav">マイページ</a><br>
+                        <a onclick="history.back()" class="header-nav">戻る</a><br>
+                        <a href="#" class="header-nav">ログアウト</a><br>
+                    </div>
+                    <nav id="desktop-menu">
+                        <a href="#" class="header-nav">マイページ</a>
+                        <a onclick="history.back()" class="header-nav">戻る</a>
+                        <a href="#" class="header-nav">ログアウト</a>
+                    </nav>
+                </div>
+            </div>
         </div>
-    </div>
+    </header>
 </body>
 
 </html>
