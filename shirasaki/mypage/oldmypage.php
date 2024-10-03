d7c43cb2fa7bc3aabc49f58dc57c6bd24500649f
Yuki shirasaki <231081@st.yoshida-g.ac.jp>
2024-10-03 12:02:43 +0900

    ベースレイアウト的なの作った

---
+++ shirasaki/mypage/oldmypage.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="mypage.css">
+
+    <title>トップページ</title>
+    </head>
+<body>
+<div class="main">
+
+
+<div class="headBanner">
+    <button class="btn-gradient-3d-simple" onclick="location.href=''">就活アプリ</button>
+    <button class="btn-gradient-3d-simple" onclick="location.href='../top/top.php'">もどる</button>
+    <button class="btn-gradient-3d-simple" onclick="location.href='../../fujii/login.php'">ログアウト</button>
+    <button class="btn-gradient-3d-simple" onclick="location.href=''">閲覧履歴</button>
+    <button class="btn-gradient-3d-simple" onclick="location.href='../quit/quit.php'">退会</button>
+</div>
+
+<div class="otu">
+    <h5>〇〇さん、お疲れ様です。</h5>
+</div>
+
+<div class="menu">
+    <div class="menu1">
+    <button class="btn-gradient-3d-simple" onclick="location.href=''">オファーリスト</button>
+    <button class="btn-gradient-3d-simple" onclick="location.href='../resume/resumeForm.php'">履歴書</button>
+    </div>
+    <div class="menu2">
+    <button class="btn-gradient-3d-simple" onclick="location.href=''">気になる企業リスト</button>
+    <button class="btn-gradient-3d-simple" onclick="location.href=''">設定</button>
+    </div>
+    </div>
+</div>
+</body>
+</html>
--- shirasaki/search/companySearch.php
+++ shirasaki/search/companySearch.php
@@ -1,57 +1,27 @@
+<?php
+include("../../db_open.php");
+include("searchCtl.php");
+$keyword = $_POST["company"];
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="../baseLayout.css">
 
-        <title>マイページ</title>
+        <title>検索結果</title>
     </head>
-    <style>
-        .block button{
-            box-shadow: 0 5px #8E9FEA;
-            border-radius: 5px;
-            width: 170px;
-            height: 60px;
-            padding: 15px;
-            box-sizing: border-box;
-            background: #C4D9FE;
-            color: #181818;
-            text-decoration: none;
-            text-align: center;
-            margin-left: 20px;
-            color: #181818;
-        }
-        .block{
-            height: 100px;
-        }
-        @media (max-width: 600px){
 
-            .right{
-            /* background-color: lightcoral; */
-            width: 100%;
-            padding-left: 100px;
-            height: 200px;
-            }
-
-            .left{
-            /* background-color: lightcoral; */
-            margin-top: 100px;
-            width: 100%;
-            padding-left: 100px;
-            height: 200px;
-            }
-        }
-    </style>
     <body>
     <div class="main">
         <header>
-            <div class="title"><h1>マイページ</h1></div>
+            <div class="title"><h1>企業検索</h1></div>
             <div class="banner">
             <button class="btn-gradient-3d-simple" onclick="location.href=''">就活アプリ</button>
-            <button class="btn-gradient-3d-simple" onclick="location.href='../top/top.php'">もどる</button>
+            <button class="btn-gradient-3d-simple" onclick="location.href='../mypage/mypage.php'">マイページ</button>
+            <button class="btn-gradient-3d-simple" onclick="location.href='history'">閲覧履歴</button>
+            <button class="btn-gradient-3d-simple" onclick="location.href='modoru'">戻る</button>
             <button class="btn-gradient-3d-simple" onclick="location.href='../../fujii/login.php'">ログアウト</button>
-            <button class="btn-gradient-3d-simple" onclick="location.href=''">閲覧履歴</button>
-            <button class="btn-gradient-3d-simple" onclick="location.href='../quit/quit.php'">退会</button>
             </div>
 
             <div class="hamburger">
@@ -78,23 +48,25 @@
         </header>
 
         <div class="mid">
-
             <div class="right">
-
                 <div class="block" id="b1">
-                <button class="btn-gradient-3d-simple" onclick="location.href=''">オファーリスト</button>
+                <form method="POST" action="">
+                <input type="text" name="company">
+                <input type="submit" value="検索">
+                </form>
+                <?PHP search($keyword,$dbh); ?>
                 </div>
                 <div class="block"> 
-                <button class="btn-gradient-3d-simple" onclick="location.href=''">気になる企業リスト</button>
+            
                 </div>
             </div>
 
             <div class="left">
                 <div class="block"> 
-                <button class="btn-gradient-3d-simple" onclick="location.href='../resume/resumeForm.php'">履歴書</button>
+                    ii
                 </div>
                 <div class="block"> 
-                <button class="btn-gradient-3d-simple" onclick="location.href=''">設定</button>
+                    ii
                 </div>
             </div>
         </div>
