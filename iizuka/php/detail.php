55235d93d91eeaaf749e8d34f465c0828f0140ee
Miki Iizuka <231048@st.yoshida-g.ac.jp>
2024-10-08 14:11:04 +0900

    企業詳細

--- iizuka/php/detail.php
+++ iizuka/php/detail.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" , href="../header.css">
-    <!-- <link rel="stylesheet" , href=""> -->
+    <link rel="stylesheet" , href="../css/detail.css">
     <meta name="viewport" content="width=device-width" />
 
     <title>企業詳細</title>
@@ -15,7 +15,7 @@
     <header>
         <div class="header">
             <h2>
-                <a href="適当にいれてね" class="web-name">job hunting</a>
+                <a href="../../shirasaki/top/top.php" class="web-name">job hunting</a>
             </h2>
             <div class="menu">
                 <div id="nav-drawer">
@@ -27,7 +27,7 @@
 
                     <!-- レスポンシブが効いてるとき -->
                     <div id="nav-content">
-                        <a href="com_mypage.php" class="header-nav">マイページ</a><br>
+                        <a href="stu_mypage.php" class="header-nav">マイページ</a><br>
                         <a href="../../komatsu/browsing.php" class="header-nav">閲覧履歴</a><br>
                         <a onclick="history.back()" class="header-nav">戻る</a><br>
                         <a href="logout.php" class="header-nav">ログアウト</a><br>
@@ -35,7 +35,7 @@
 
                     <!-- 通常メニュー -->
                     <nav id="desktop-menu">
-                        <a href="com_mypage.php" class="header-nav">マイページ</a>
+                        <a href="stu_mypage.php" class="header-nav">マイページ</a>
                         <a href="../../komatsu/browsing.php" class="header-nav">閲覧履歴</a>
                         <a onclick="history.back()" class="header-nav">戻る</a>
                         <a href="logout.php" class="header-nav" id="logout">ログアウト</a>
@@ -63,6 +63,8 @@
     echo <<<___EOF
     <h3>$rec2[com_name]</h3>
     
+    <div class='info'>
+
     <h4>企業理念</h4>
     <p>$rec[com_rinen]</p>
     
@@ -73,47 +75,85 @@
     
     ___EOF;
 
-    if (isset($rec['free1'])) {
+    if (empty($rec['free1'])) {
+        echo "<p hidden>$rec[free1]</p>";
+    } else {
         echo "<p>$rec[free1]</p>";
     }
-    if (isset($rec['free2'])) {
+    if (empty($rec['free2'])) {
+        echo "<p hidden>$rec[free2]</p>";
+    } else {
         echo "<p>$rec[free2]</p>";
     }
-    if (isset($rec['free3'])) {
-        echo "<p>$rec[free3]</p>";
-    }
-
-
-    echo "<h4>イベント情報</h4>";
-
-    if (isset($rec['event1'])) {
-        echo "<ul>";
-        echo "<li><p>$rec[event1]</p></ul>";
-        echo "</ul>";
-    }else{
-
-    }
-    if (isset($rec['event2'])) {
-        echo "<ul>";
-        echo "<li><p>$rec[event2]</p></ul>";
-        echo "</ul>";
-    }else{
-
-    }
-    if (isset($rec['event3'])) {
-        echo "<ul>";
-        echo "<li><p>$rec[event3]</p></li>";
-        echo "</ul>";
-    }else{
-        
-    }
+    // if (empty($rec['free3'])) {
+    //     echo "<p hidden>$rec[free3]</p>";
+    // }else{
+    //     echo "<p>$rec[free3]</p>";
+    // }
 
     echo <<<___EOF
     <p>$rec2[com_address]</p>
     <p>$rec2[com_tell]</p>
     <p>$rec2[com_mail]</p>
-
     ___EOF;
+
+    echo "<h4>イベント情報</h4>";
+
+    // null判定。tuleだと出力なし。
+    if (empty($rec['event1'])) {
+        echo "<ul hidden>";
+        echo "<p hidden>$rec[event1]</p>";
+        echo "</ul>";
+    } else {
+        // falseだとform　日付クリックでイベント予約に遷移
+        echo "<ul>";
+        // echo "<p>‣$rec[event1]</p>";
+
+        echo <<<___EOF
+            <form action="../../komatsu/eventbooking.php" method="post">
+                <input type="hidden" name="delete" value="$rec[eventdata1]">
+                <p>‣$rec[event1]<input type="submit" name="submit" value = "$rec[eventdata1]"></p>
+            </form>
+
+         ___EOF;
+        echo "</ul>";
+    }
+    if (isset($rec['event2'])) {
+        echo "<ul hidden>";
+        echo "<p hidden>$rec[event2]</p>";
+        echo "</ul>";
+    } else {
+        echo "<ul>";
+        echo <<<___EOF
+            <form action="../../komatsu/eventbooking.php" method="post">
+                <input type="hidden" name="delete" value="$rec[eventdata2]">
+                <p>‣$rec[event2]<input type="submit" name="submit" value = "$rec[eventdata2]"></p>
+            </form>
+
+         ___EOF;
+        // echo "<p>‣$rec[event2]</p>";
+        // echo "<a>$rec[eventdata2]</a>";
+        echo "</ul>";
+    }
+    if (isset($rec['event3'])) {
+        echo "<ul hidden>";
+        echo "<p hidden>$rec[event3]</p>";
+        echo "</ul>";
+    } else {
+        echo "<ul>";
+        echo <<<___EOF
+            <form action="../../komatsu/eventbooking.php" method="post">
+                <input type="hidden" name="delete" value="$rec[eventdata3]">
+                <p>‣$rec[event3]<input type="submit" name="submit" value = "$rec[eventdata3]"></p>
+            </form>
+
+         ___EOF;
+        // echo "<p>‣$rec[event3]</p>";
+        // echo "<a>$rec[eventdata3]</a>";
+        echo "</ul>";
+    }
+
+    echo "</div>";
 
 
 
