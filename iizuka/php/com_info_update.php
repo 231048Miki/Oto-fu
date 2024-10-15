9621dc4bc6227a7aedcabe1c192556b8db67e249
Miki Iizuka <231048@st.yoshida-g.ac.jp>
2024-10-15 09:51:12 +0900

    エラー処理

--- iizuka/php/com_info_update.php
+++ iizuka/php/com_info_update.php
@@ -23,36 +23,38 @@
     <h2 class="page-name">情報更新</h2>
 
     <?php
-    // session_start();
-    // if (!isset($_SESSION['login'])) {
-    //     header("Location:");
-    //     // セッション追加頼む
-    //     exit();
-    // }
+    session_start();
+
+    if (!isset($_SESSION['login']) && !isset($_SESSION['com_id'])) {
+        header("Location:../../fujii/login.php");
+        exit();
+    } else {
+        $userid = $_SESSION['com_id'];
+        // echo $userid;  
+    }
     function fileSave($com_name, $manager, $mail, $tell, $address,  $pass) {}
 
     //idに基づいて表示
-    $userid = $_SESSION['id'];
+    // $userid = $_SESSION['id'];
     include '../../db_open.php';
 
-    $sql2 = "SELECT * FROM company_table WHERE com_id = $userid";
+    $sql2 = "SELECT * FROM company_table WHERE com_id = $_SESSION[com_id]";
     $sql_res2 = $dbh->query($sql2);
     $rec2 = $sql_res2->fetch();
 
     if ($_SERVER["REQUEST_METHOD"] != "POST") {
         echo <<<___EOF___
-            <form method="POST" action="" enctype="multipart/form-data" class="com_form">
-            <input type="hidden" name="MAX_FILE_SIZE" value="1500000" /> 
+            <form method="POST" action="" class="com_form">
             <h3>企業名：<input type="text" name="com_name" value="{$rec2['com_name']}" required></h3>
             <h3>採用担当者名：<input type="text" name="cmanager" value="{$rec2['manager']}" required></h3>
-            <h3>メールアドレス：<input type="text" name="mail" value="{$rec2['mail']}" required></h3>
-            <h3>電話番号：<input type="text" name="tell" value="{$rec2['tell']}" required></h3>
-            <h3>所在地：<input type="text" name="address" value="{$rec2['address']}" required></h3>
-            <h3>パスワード：<input type="password" name="pass" value="{$rec2['pass']}" required></h3>
-            <h3>再入力：<input type="password" name="pass" value="{$rec2['pass']}" required></h3>
+            <h3>メールアドレス：<input type="text" name="mail" value="{$rec2['com_mail']}" required></h3>
+            <h3>電話番号：<input type="text" name="tell" value="{$rec2['com_tell']}" required></h3>
+            <h3>所在地：<input type="text" name="address" value="{$rec2['com_address']}" required></h3>
             <input type="submit" value="更新">
         </form>
 ___EOF___;
+// <h3>パスワード：<input type="password" name="pass" value="{$rec2['pass']}" required></h3>
+// <h3>再入力：<input type="password" name="pass" value="{$rec2['pass']}" required></h3>
     } else {
         $name = $_POST['name'];
         $manager = $_POST['manager'];
