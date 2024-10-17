26768ead3e037f5189ad5a2377a9886f037e112a
Miki Iizuka <231048@st.yoshida-g.ac.jp>
2024-10-17 14:49:33 +0900

    なんかコメント消えてたんだが

--- komatsu/browsing.php
+++ komatsu/browsing.php
@@ -4,14 +4,16 @@
 <head>
   <meta charset="UTF-8">
   <title>閲覧履歴</title>
-  <link rel="stylesheet" href="../iizuka/header.css">
-  <link rel="stylesheet" href="browsing.css">
+  <link rel="stylesheet" , href="../iizuka/header.css">
+  <link rel="stylesheet" , href="browsing.css">
+  <!-- cssファイル作ったのでそっちにデザイン系移しました　飯塚 -->
+  <!-- クラス名はわかりやすいもののほうがいいと思います。aaaとかわからん、、、 -->
   <meta name="viewport" content="width=device-width" />
 </head>
 
 <body>
 
-  <!-- ヘッダー追加 -->
+<!-- ヘッダー追加 -->
   <header>
     <div class="header">
       <h2>
@@ -19,10 +21,13 @@
       </h2>
       <div class="menu">
         <div id="nav-drawer">
+          <!-- ハンバーガーメニュー開いたときの挙動。これないと機能しません -->
           <input id="nav-input" type="checkbox" class="nav-unshown">
+          <!-- 三本線 -->
           <label id="nav-open" for="nav-input" class="nav-unshown"><span></span></label>
           <label class="nav-unshown" id="nav-close" for="nav-input"></label>
 
+          <!-- レスポンシブが効いてるとき -->
           <div id="nav-content">
             <a onclick="history.back()" class="header-nav">戻る</a><br>
             <a href="../iizuka/logout.php" class="header-nav">ログアウト</a><br>
@@ -30,6 +35,7 @@
             <a href="../shirasaki/mypage/mypage.php" class="header-nav">マイページ</a><br>
           </div>
 
+          <!-- 通常メニュー -->
           <nav id="desktop-menu">
             <a onclick="history.back()" class="header-nav">戻る</a>
             <a href="../iizuka/logout.php" class="header-nav">ログアウト</a>
@@ -44,11 +50,12 @@
   <h3 class="sub-header">閲覧履歴</h3>
   <div class="history-P">
     <div id="wrapper" class="histry-C">
-      <?php
-      include "../db_open.php";
+    <?php
+include "../db_open.php";
 
-      try {
-          $sql = 'SELECT c.com_id, c.com_name, i.com_rinen 
+try {
+    
+  $sql = 'SELECT c.com_id, c.com_name, i.com_rinen 
                   FROM company_table c 
                   JOIN cominfo_table i ON c.com_id = i.com_id
                   ORDER BY c.com_id DESC;'; 
